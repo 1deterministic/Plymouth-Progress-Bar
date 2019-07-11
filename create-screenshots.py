efc509b16bbdbd4dd3958093e3aa381f1d95084b
1deterministic <22532656+1deterministic@users.noreply.github.com>
2019-07-11 16:30:43 -0300

    Minor adjustments

--- create-screenshots.py
+++ create-screenshots.py
@@ -55,15 +55,15 @@ def create_screenshots(path, resolutions):
 if __name__ == "__main__":
     path = ""
     resolutions = [
-        {"width": 1024, "height": 768},
-        {"width": 1280, "height": 720},
-        {"width": 1360, "height": 768},
-        {"width": 1366, "height": 768},
-        {"width": 1600, "height": 900},
-        {"width": 1920, "height": 1080},
-        {"width": 2560, "height": 1080},
-        {"width": 2560, "height": 1440},
-        {"width": 3840, "height": 2160}
+        # {"width": 3840, "height": 2160},
+        # {"width": 2560, "height": 1440},
+        # {"width": 2560, "height": 1080},
+        # {"width": 1920, "height": 1080},
+        # {"width": 1600, "height": 900},
+        # {"width": 1366, "height": 768},
+        # {"width": 1360, "height": 768},
+        # {"width": 1280, "height": 720},
+        {"width": 1024, "height": 768}
     ]
 
     # find args
